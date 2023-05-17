7aed89fb841fd4484a3a28120a6b4d1b8545737b
gustavogss <gustavogss.dev@gmail.com>
2023-05-17 19:29:18 -0300

    frontend

---
+++ web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Botao } from '@/components/Botao'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+    <h1>Spacetime Web</h1>
+    <Botao />
+    </>
+  )
+}
